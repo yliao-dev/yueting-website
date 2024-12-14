d2b5d1348c1c8fbab190a281ed281119fdf663f7
yueting <ytliaorpi@gmail.com>
2024-12-14 23:05:37 +0100

    Improve expand bar

--- personal-website/src/components/layouts/Navbar.tsx
+++ personal-website/src/components/layouts/Navbar.tsx
@@ -1,43 +1,50 @@
+import { useEffect, useState } from "react";
 import logo from "../../assets/images/logo.png";
-import { NavLink } from "react-router-dom";
-
+import { NavLink, useLocation } from "react-router-dom";
+import ReorderIcon from "@mui/icons-material/Reorder";
 const Navbar = () => {
   const linkClass = ({ isActive }: { isActive: boolean }) =>
-    isActive
-      ? "active-link" // Define active link style in CSS
-      : "inactive-link"; // Define inactive link and hover styles in CSS
+    isActive ? "active-link" : "inactive-link";
 
+  const [expandNavbar, setExpandNavbar] = useState(false);
+  const location = useLocation();
+
+  useEffect(() => {
+    setExpandNavbar(false);
+  }, [location]);
   return (
-    <nav className="navbar">
-      <div className="container">
-        <div className="nav-content">
-          {/* Logo */}
-          <NavLink className="logo" to="/">
-            <img className="logo-img" src={logo} />
-          </NavLink>
-
-          {/* Links */}
-          <div className="nav-links">
-            <NavLink
-              to="/about"
-              className={({ isActive }) => linkClass({ isActive })}
-            >
-              about
-            </NavLink>
-            <NavLink
-              to="/portfolio"
-              className={({ isActive }) => linkClass({ isActive })}
-            >
-              portfolio
-            </NavLink>
-            <NavLink
-              to="/blog"
-              className={({ isActive }) => linkClass({ isActive })}
-            >
-              blog
-            </NavLink>
-          </div>
-        </div>
+    <nav className="navbar" id={expandNavbar ? "open" : "close"}>
+      <div className="toggleButton">
+        <button
+          onClick={() => {
+            setExpandNavbar((prev) => !prev);
+          }}
+        >
+          <ReorderIcon />
+        </button>
+      </div>
+      <NavLink className="logo" to="/">
+        <img className="logo-img" src={logo} />
+      </NavLink>
+      <div className="nav-links">
+        <NavLink
+          to="/about"
+          className={({ isActive }) => linkClass({ isActive })}
+        >
+          about
+        </NavLink>
+        <NavLink
+          to="/portfolio"
+          className={({ isActive }) => linkClass({ isActive })}
+        >
+          portfolio
+        </NavLink>
+        <NavLink
+          to="/blog"
+          className={({ isActive }) => linkClass({ isActive })}
+        >
+          blog
+        </NavLink>
       </div>
     </nav>
   );
