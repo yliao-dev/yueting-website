542f3b48eee40cf2bf5dc1d7da0228e0c35f5178
yueting <ytliaorpi@gmail.com>
2024-12-15 18:47:10 +0100

    Some progress

--- personal-website/src/components/layouts/Navbar.tsx
+++ personal-website/src/components/layouts/Navbar.tsx
@@ -7,47 +7,66 @@ const Navbar = () => {
     isActive ? "active-link" : "inactive-link";
 
   const [expandNavbar, setExpandNavbar] = useState(false);
+
+  const toggleNavbar = () => {
+    setExpandNavbar((prev) => !prev);
+  };
+
   const location = useLocation();
 
   useEffect(() => {
     setExpandNavbar(false);
   }, [location]);
-  return (
-    <nav className="navbar" id={expandNavbar ? "open" : "close"}>
-      <NavLink className="logo" to="/">
-        <img className="logo-img" src={logo} />
-      </NavLink>
-      <div className="links">
-        <NavLink
-          to="/about"
-          className={({ isActive }) => linkClass({ isActive })}
-        >
-          about
-        </NavLink>
-        <NavLink
-          to="/portfolio"
-          className={({ isActive }) => linkClass({ isActive })}
-        >
-          portfolio
-        </NavLink>
-        <NavLink
-          to="/blog"
-          className={({ isActive }) => linkClass({ isActive })}
-        >
-          blog
-        </NavLink>
-      </div>
 
-      <div className="toggleButton">
-        <button
-          onClick={() => {
-            setExpandNavbar((prev) => !prev);
-          }}
-        >
-          <ReorderIcon />
-        </button>
-      </div>
-    </nav>
+  // TODO: Dropdown toggle list, for everylink, add a row to the list
+  return (
+    <>
+      <nav className="navbar" id={expandNavbar ? "open" : "close"}>
+        <NavLink className="logo" to="/">
+          <img className="logo-img" src={logo} />
+        </NavLink>
+        <div className="links">
+          <NavLink
+            to="/about"
+            className={({ isActive }) => linkClass({ isActive })}
+          >
+            about
+          </NavLink>
+          <NavLink
+            to="/portfolio"
+            className={({ isActive }) => linkClass({ isActive })}
+          >
+            portfolio
+          </NavLink>
+          <NavLink
+            to="/blog"
+            className={({ isActive }) => linkClass({ isActive })}
+          >
+            blog
+          </NavLink>
+        </div>
+
+        <div className="toggleButton">
+          <button onClick={toggleNavbar}>
+            <ReorderIcon />
+          </button>
+        </div>
+      </nav>
+
+      {expandNavbar && (
+        <div className="dropdown-links">
+          <NavLink to="/about" className="dropdown-link">
+            About
+          </NavLink>
+          <NavLink to="/portfolio" className="dropdown-link">
+            Portfolio
+          </NavLink>
+          <NavLink to="/blog" className="dropdown-link">
+            Blog
+          </NavLink>
+        </div>
+      )}
+    </>
   );
 };
 
