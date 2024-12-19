6e4b1f465685d7044bf6b0c8bdb9158e7a395f4f
yueting <ytliaorpi@gmail.com>
2024-12-19 19:02:22 +0100

    Update photographyItemData.tsx

--- personal-website/src/data/blog/photography/photographyItemData.tsx
+++ personal-website/src/data/blog/photography/photographyItemData.tsx
@@ -1,5 +1,5 @@
-import DSCF00570 from "../../../assets/images/blog/photography/DSC00570.jpg";
 import DSCF00008 from "../../../assets/images/blog/photography/DSCF00008.jpg";
+import DSCF00570 from "../../../assets/images/blog/photography/DSC00570.jpg";
 import DSCF0408 from "../../../assets/images/blog/photography/DSCF0408.jpg";
 
 export const PhotographyItemData = [
