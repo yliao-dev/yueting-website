6d64d01f4f5e5684df924389209751ee5fc5d8a0
yueting <ytliaorpi@gmail.com>
2024-12-20 20:44:25 +0100

    Add author section

--- personal-website/src/components/blog/blogPost.tsx
+++ personal-website/src/components/blog/blogPost.tsx
@@ -8,14 +8,21 @@ type BlogPostProp = {
 };
 
 function BlogPost({ name, title, image }: BlogPostProp) {
+  const author = "Yueting Liao";
+  const date = "Dec 20, 2024";
   return (
     <>
       <div className="blog__post">
-        <img src={image} alt={title} className="blog__post__image" />
+        <img src={image} className="blog__post__image" />
         <Link to={`${name}`} className="blog__post__title">
           <h1>{title}</h1>
         </Link>
         <p className="blog__post__desc">{name}</p>
+        <section className="blog__post__author">
+          <span className="blog__post__author-icon">🖊️</span>
+          <span className="blog__post__author-name">{author}</span>
+          <span className="blog__post__author-date">{date}</span>
+        </section>
       </div>
     </>
   );
