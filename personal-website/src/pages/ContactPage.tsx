1e76bbd3df12ee4112c8723bbeda3c1c33e22b4e
yueting <ytliaorpi@gmail.com>
2024-12-22 19:00:33 +0100

    Update ContactPage.tsx

--- personal-website/src/pages/ContactPage.tsx
+++ personal-website/src/pages/ContactPage.tsx
@@ -1,5 +1,3 @@
-import { LinkedIn, GitHub, Instagram } from "@mui/icons-material";
-import DSCF4076 from "../assets/images/DSCF4076.jpg";
 import LineBreak from "../components/LineBreak";
 import "../styles/contact.css";
 import SocialMediaIcons from "../components/SocialMediaIcons";
