aa7ab3eb23959dde9290c10ad5487ae96a2e7b05
yueting <ytliaorpi@gmail.com>
2025-03-17 20:37:38 +0100

    Small home page UI

--- personal-website/src/pages/HomePage.tsx
+++ personal-website/src/pages/HomePage.tsx
@@ -5,33 +5,41 @@ const HomePage = () => {
   return (
     <>
       <section className="home">
+        <div className="home__intro">
+          <h1>About Me</h1>
+        </div>
         <img
           src="/images/photography/DSCF9770.jpg"
           className="home__intro__image"
           alt="Home Intro"
         />
       </section>
-      <LineBreak variant="plain" />
-      <div className="home__intro">
-        <h1>Welcome to My Website</h1>
-      </div>
 
-      <LineBreak variant="plain" />
-
-      <section className="contact__my__skill">
+      <section className="home_my_skill">
         <h1>My Skills</h1>
+        <img
+          src="/images/photography/DSCF9770.jpg"
+          className="home__intro__image"
+          alt="Home Intro"
+        />
       </section>
 
-      <LineBreak variant="plain" />
-
-      <section className="contact__my__story">
+      <section className="home_my_story">
         <h1>My Storys</h1>
+        <img
+          src="/images/photography/DSCF9770.jpg"
+          className="home__intro__image"
+          alt="Home Intro"
+        />
       </section>
 
-      <LineBreak variant="plain" />
-
-      <section className="contact__my__places">
+      <section className="home_interactive_map">
         <h1>Interactive world Map</h1>
+        <img
+          src="/images/photography/DSCF9770.jpg"
+          className="home__intro__image"
+          alt="Home Intro"
+        />
       </section>
     </>
   );
