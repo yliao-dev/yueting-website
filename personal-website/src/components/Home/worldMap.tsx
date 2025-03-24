a421586ba1ddb642b084c0ea069138596d086f21
yueting <ytliaorpi@gmail.com>
2025-03-24 20:44:01 +0100

    Update worldMap.tsx

--- personal-website/src/components/Home/worldMap.tsx
+++ personal-website/src/components/Home/worldMap.tsx
@@ -1,18 +1,15 @@
 import { useEffect, useState } from "react";
 import { csv } from "d3-fetch";
-import { scaleLinear } from "d3-scale";
 import { ComposableMap, Geographies, Geography } from "react-simple-maps";
 
 const geoUrl = "/features.json";
 const csvPath = "/highlightedRegions.csv";
 
-const colorScale = scaleLinear<string>()
-  .domain([0.29, 0.68])
-  .range(["#ffedea", "#ff5233"]);
-
 type CountryData = {
   ISO3: string;
-  [year: string]: number | string;
+  Name: string;
+  Lived: number;
+  Travelled: number;
 };
 
 type Props = {
@@ -25,55 +22,72 @@ const MapChart = ({ setTooltipContent }: Props) => {
   useEffect(() => {
     csv(csvPath, (row) => ({
       ISO3: row.ISO3,
-      "2025": +row["2025"], // convert to number
+      Name: row.Name,
+      Lived: +row.Lived,
+      Travelled: +row.Travelled,
     })).then((parsed) => {
       setData(parsed);
     });
   }, []);
 
   return (
-    <ComposableMap
-      projectionConfig={{
-        rotate: [-10, 0, 0],
-        scale: 147,
-      }}
-    >
-      {data.length > 0 && (
-        <Geographies geography={geoUrl}>
-          {({ geographies }) =>
-            geographies.map((geo) => {
-              const d = data.find((s) => s.ISO3 === geo.id);
-              const fillColor = d ? colorScale(d["2025"] as number) : "#F5F4F6";
-              return (
-                <Geography
-                  key={geo.rsmKey}
-                  geography={geo}
-                  onMouseEnter={() => {
-                    setTooltipContent(`${geo.properties.name}`);
-                  }}
-                  onMouseLeave={() => {
-                    setTooltipContent("");
-                  }}
-                  style={{
-                    default: {
-                      fill: fillColor,
-                    },
-                    hover: {
-                      fill: "#0072B1", // hover color
-                      outline: "none",
-                    },
-                    // pressed: {
-                    //   fill: "#E42",
-                    //   outline: "none",
-                    // },
-                  }}
-                />
-              );
-            })
-          }
-        </Geographies>
-      )}
-    </ComposableMap>
+    <>
+      <ComposableMap
+        projection="geoMercator"
+        projectionConfig={{
+          scale: 120, // adjust to fit your view
+          center: [0, 0],
+        }}
+      >
+        {data.length > 0 && (
+          <Geographies geography={geoUrl}>
+            {({ geographies }) =>
+              geographies.map((geo) => {
+                const d = data.find((s) => s.ISO3 === geo.id);
+
+                let fillColor = "#F5F4F6"; // default gray
+                if (d) {
+                  if (d.Lived === 1) fillColor = "#0072b1"; // dark blue
+                  else if (d.Travelled === 1) fillColor = "#89c2d9"; // light blue
+                }
+
+                return (
+                  <Geography
+                    key={geo.rsmKey}
+                    geography={geo}
+                    data-tooltip-id="map-tooltip"
+                    onMouseEnter={() => {
+                      if (d) {
+                        setTooltipContent(d.Name);
+                      } else {
+                        setTooltipContent(geo.properties.name);
+                      }
+                    }}
+                    onMouseLeave={() => {
+                      setTooltipContent("");
+                    }}
+                    style={{
+                      default: {
+                        fill: fillColor,
+                        outline: "none",
+                      },
+                      hover: {
+                        fill: "#d2b48c", // green on hover
+                        outline: "none",
+                      },
+                      // pressed: {
+                      //   fill: "#38b000",
+                      //   outline: "none",
+                      // },
+                    }}
+                  />
+                );
+              })
+            }
+          </Geographies>
+        )}
+      </ComposableMap>
+    </>
   );
 };
 
