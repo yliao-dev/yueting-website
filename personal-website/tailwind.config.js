33cf6f34f54fa2e0c61582dce4c1cd9dcc4fd6f0
yueting <ytliaorpi@gmail.com>
2025-03-26 14:27:14 +0100

    Update tailwind.config.js

--- personal-website/tailwind.config.js
+++ personal-website/tailwind.config.js
@@ -2,14 +2,7 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      fontFamily: {
-        sans: ["Roboto", "sans-serif"],
-      },
-      gridTemplateColumns: {
-        "70/30": "70% 28%",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
