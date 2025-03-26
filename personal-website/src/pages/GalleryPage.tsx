4b2acc2f0484d163eaa1939211a0bd6985dfedea
yueting <ytliaorpi@gmail.com>
2025-03-26 12:09:36 +0100

    Update GalleryPage.tsx

--- personal-website/src/pages/GalleryPage.tsx
+++ personal-website/src/pages/GalleryPage.tsx
@@ -9,9 +9,10 @@ const GalleryPage = () => {
           <h1>Moments</h1>
           <p>
             A collection of moments, perspectives, and stories—captured through
-            my lens. started photography in November 2023 with my first camera,
-            the FUJIFILM X-T5. Shot using the XF 35mm f/1.4, XF 18–55mm f/2.8–4,
-            and XF 70–300mm f/4–5.6.
+            my lens. I started photography in November 2023 with my first
+            camera, the FUJIFILM X-T5. These images were shot using the XF 35mm
+            f/1.4, XF 18–55mm f/2.8–4, and XF 70–300mm f/4–5.6 lenses, and
+            edited in Capture One.
           </p>
         </section>
         <section>
