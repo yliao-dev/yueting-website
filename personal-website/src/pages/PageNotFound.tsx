46b5548afedcc5a3bb17a05aab2b719a4211b391
yueting <ytliaorpi@gmail.com>
2025-03-26 18:01:41 +0100

    Update 404 pag

--- personal-website/src/pages/PageNotFound.tsx
+++ personal-website/src/pages/PageNotFound.tsx
@@ -1,19 +1,20 @@
 import { FaExclamationTriangle } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
-const PageNotFound: React.FC = () => {
+const PageNotFound = () => {
   return (
-    <section className="text-center flex flex-col justify-center items-center h-96">
-      <FaExclamationTriangle className="text-yellow-400 fa-4x mb-4" />
-      <h1 className="text-6xl font-bold mb-4">404 Not Found</h1>
-      <p className="text-xl mb-5">This page does not exist</p>
-      <Link
-        to="/"
-        className="text-white bg-indigo-700 hover:bg-indigo-900 rounded-md px-3 py-2 mt-4"
-      >
-        Go Back
-      </Link>
-    </section>
+    <>
+      <div className="pageNotFound">
+        <section className="pageNotFound__intro">
+          <div className="pageNotFound__text">
+            <h1>404 Not Found</h1>
+            <p>This page does not exist</p>
+            <FaExclamationTriangle />
+            <Link to="/">Go Back</Link>
+          </div>
+        </section>
+      </div>
+    </>
   );
 };
 
