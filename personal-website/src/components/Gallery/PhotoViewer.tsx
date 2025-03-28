e62fabd754ec9aa1ea5f638e6c4068f804c0d3d2
yueting <ytliaorpi@gmail.com>
2025-03-28 22:25:18 +0100

    Remove used close button for photo modal

--- personal-website/src/components/Gallery/PhotoViewer.tsx
+++ personal-website/src/components/Gallery/PhotoViewer.tsx
@@ -5,9 +5,6 @@ const PhotoViewer = ({ imageUrl, onClose }: PhotoViewerProps) => {
     <>
       <div className="modal-overlay" onClick={onClose}>
         <div className="modal-content" onClick={(e) => e.stopPropagation()}>
-          {/* <button className="close-button" onClick={onClose}>
-            &times;
-          </button> */}
           <img src={imageUrl} alt="Full view" className="modal-image" />
         </div>
       </div>
