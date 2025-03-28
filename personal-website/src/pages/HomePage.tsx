14c3228b8e2c07d0e09bce106541540092e41b49
yueting <ytliaorpi@gmail.com>
2025-03-28 11:25:58 +0100

    test adding class

--- personal-website/src/pages/HomePage.tsx
+++ personal-website/src/pages/HomePage.tsx
@@ -14,7 +14,7 @@ const HomePage = () => {
     <>
       <div className="home">
         <section className="home__about">
-          <div>
+          <div className="home__about__text">
             <h1>About</h1>
             <h2>
               Developer <span className="home__about__text__divider">|</span>
