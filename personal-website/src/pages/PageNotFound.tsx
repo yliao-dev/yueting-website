42696bd15491b7c2b3bb533141e6872c3279382f
yueting <ytliaorpi@gmail.com>
2025-03-29 19:39:57 +0100

    404 Page update

--- personal-website/src/pages/PageNotFound.tsx
+++ personal-website/src/pages/PageNotFound.tsx
@@ -5,31 +5,17 @@ import bison from "../assets/images/bison.jpg";
 const PageNotFound = () => {
   return (
     <div className="pageNotFound">
-      <section></section>
       <section className="pageNotFound__intro">
-        <div className="pageNotFound__text">
-          <h1>404 Not Found</h1>
-          <p>
-            Hmm, looks like you’ve wandered into uncharted territory. Don’t
-            worry though, my trusty horse will guide you back to safety!
-          </p>
-          <FaExclamationTriangle className="pageNotFound__warning" />
-          <Link to="/" className="pageNotFound__back-button">
-            Back with Bison
-          </Link>
-        </div>
-      </section>
-      <section className="pageNotFound__bison">
-        <img src={bison} alt="Bison in 404" />
+        <h1>⚠️ 404 Not Found</h1>
         <p>
-          Name: Bison
-          <br />
-          Nationality: Polish
-          <br />
-          Species: Horse
-          <br />
-          Favorite Food: Grass
+          Hmm, looks like you’ve wandered into uncharted territory. Don’t worry
+          though, my trusty horse will guide you back to safety!
         </p>
+        <img src={bison} alt="Bison in 404" />
+
+        <Link to="/" className="pageNotFound__back-button">
+          Back with Bison
+        </Link>
       </section>
     </div>
   );
