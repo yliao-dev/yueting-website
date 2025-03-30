497dfdd03d0e3e7d371267ae6d6c4956fd6f19bc
yueting <ytliaorpi@gmail.com>
2025-03-30 11:52:04 +0200

    Adding animation to gallery item

--- personal-website/src/components/Gallery/PhotoMasonry.tsx
+++ personal-website/src/components/Gallery/PhotoMasonry.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { PhotoItemProps } from "./PhotoTypes";
 import PhotoViewer from "./PhotoViewer";
+import { useScrollIndex } from "../../hooks/useScrollIndex";
 
 interface PhotoMasonryProps {
   photoData: PhotoItemProps[];
@@ -8,6 +9,13 @@ interface PhotoMasonryProps {
 
 const PhotoMasonry = ({ photoData }: PhotoMasonryProps) => {
   const [selectedImage, setSelectedImage] = useState<string | null>(null);
+
+  const [visibleIndex, setVisibleIndex] = useState<number | null>(null);
+  const scrollRefs = useScrollIndex(setVisibleIndex, {
+    threshold: 0.1,
+    rootMargin: "-20% 0px -20% 0px",
+  });
+
   const openViewer = (imageUrl: string) => {
     setSelectedImage(imageUrl);
   };
@@ -20,7 +28,13 @@ const PhotoMasonry = ({ photoData }: PhotoMasonryProps) => {
         {photoData.map((photo) => (
           <div
             key={photo.id}
-            className="masonry__item"
+            className={`masonry__item ${
+              visibleIndex === 0 ? "animate-fade-slide-up" : ""
+            }`}
+            data-index={0}
+            ref={(el) => {
+              if (el) scrollRefs.current[0] = el;
+            }}
             onClick={() => openViewer(photo.image)}
           >
             <img src={photo.image} alt={photo.title} />
