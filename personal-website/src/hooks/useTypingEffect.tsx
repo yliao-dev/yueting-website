85be9613e3745534ff10d376e3aef2fffed7b1d6
yueting <ytliaorpi@gmail.com>
2025-03-30 15:55:12 +0200

    Create useTypingEffect.tsx

---
+++ personal-website/src/hooks/useTypingEffect.tsx
@@ -0,0 +1,18 @@
+import { useEffect, useState } from "react";
+
+export const useTypingEffect = (text: string, speed = 50) => {
+  const [displayedText, setDisplayedText] = useState("");
+
+  useEffect(() => {
+    let i = 0;
+    const interval = setInterval(() => {
+      setDisplayedText(text.slice(0, i + 1));
+      i++;
+      if (i === text.length) clearInterval(interval);
+    }, speed);
+
+    return () => clearInterval(interval);
+  }, [text, speed]);
+
+  return displayedText;
+};
