0c00daf07c5feed9dc551f05412d12e04b6ef4e6
yueting <ytliaorpi@gmail.com>
2025-03-30 20:19:41 +0200

    Basic project page structure

--- personal-website/src/pages/ProjectPage.tsx
+++ personal-website/src/pages/ProjectPage.tsx
@@ -1,25 +1,35 @@
 import { useParams } from "react-router-dom";
 import { ProjectData } from "../data/ProjectData";
+import PageNotFound from "./PageNotFound";
+import LineBreak from "../components/LineBreak";
 
 const ProjectPage = () => {
   const { id } = useParams();
   const project = ProjectData.find((p) => String(p.id) === id);
 
-  if (!project) return <div>Project not found.</div>;
+  if (!project) return <PageNotFound />;
 
   return (
     <>
-      <div className="project-page">
-        <h1>{project.name}</h1>
-        <img src={project.image} alt={project.name} className="project-image" />
-        <p className="project-description">{project.description}</p>
-        <div className="project-tags">
-          {project.tags?.map((tag) => (
-            <span key={tag} className="tag">
-              {tag}
-            </span>
-          ))}
-        </div>
+      <div className="project">
+        <section className="project__text">
+          <h1>{project.name}</h1>
+          <p>{project.description}</p>
+          <div>
+            {project.tags?.map((tag) => (
+              <span key={tag} className="project__tag">
+                {tag}
+              </span>
+            ))}
+          </div>
+        </section>
+        <section className="project__images">
+          <img
+            src="/images/gallery/DSCF7886.jpg"
+            className="project__intro__image"
+            alt="project"
+          />
+        </section>
       </div>
     </>
   );
