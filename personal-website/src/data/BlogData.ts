02f5b5f40e53c7de249808413286a9c9e7951b23
yueting <ytliaorpi@gmail.com>
2025-03-31 15:59:54 +0200

    Update BlogData.ts

--- personal-website/src/data/BlogData.ts
+++ personal-website/src/data/BlogData.ts
@@ -24,53 +24,53 @@ export const BlogData = [
     category: "kendo",
   },
 
-  // Fitness
-  {
-    id: "4",
-    title: "Fitness New York",
-    description:
-      "In the heart of the forest, tall trees stand like guardians of nature.",
-    image: "src/assets/images/blogPost1.jpg",
-    category: "fitness",
-  },
-  {
-    id: "5",
-    title: "Fitness Florida",
-    description: "The sun rises slowly over the hills.",
-    image: "src/assets/images/blogPost1.jpg",
-    category: "fitness",
-  },
-  {
-    id: "6",
-    title: "Fitness California",
-    description:
-      "The forest is a peaceful place filled with tall trees and soft sounds.",
-    image: "src/assets/images/blogPost1.jpg",
-    category: "fitness",
-  },
-
   // Photography
   {
-    id: "7",
+    id: "74",
     title: "Photography New York",
     description:
       "In the heart of the forest, tall trees stand like guardians of nature.",
-    image: "src/assets/images/blogPost1.jpg",
+    image: "src/assets/images/blogPost2.jpg",
     category: "photography",
   },
   {
-    id: "8",
+    id: "5",
     title: "Photography Florida",
     description: "The sun rises slowly over the hills.",
-    image: "src/assets/images/blogPost1.jpg",
+    image: "src/assets/images/blogPost2.jpg",
     category: "photography",
   },
   {
-    id: "9",
+    id: "6",
     title: "Photography California",
     description:
       "The forest is a peaceful place filled with tall trees and soft sounds.",
-    image: "src/assets/images/blogPost1.jpg",
+    image: "src/assets/images/blogPost2.jpg",
     category: "photography",
+  },
+
+  // Fitness
+  {
+    id: "7",
+    title: "Fitness New York",
+    description:
+      "In the heart of the forest, tall trees stand like guardians of nature.",
+    image: "src/assets/images/blogPost3.jpg",
+    category: "fitness",
+  },
+  {
+    id: "8",
+    title: "Fitness Florida",
+    description: "The sun rises slowly over the hills.",
+    image: "src/assets/images/blogPost3.jpg",
+    category: "fitness",
+  },
+  {
+    id: "9",
+    title: "Fitness California",
+    description:
+      "The forest is a peaceful place filled with tall trees and soft sounds.",
+    image: "src/assets/images/blogPost3.jpg",
+    category: "fitness",
   },
 ];
