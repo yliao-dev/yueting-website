123c1c468a532a9006e57420dc0ffa6f717e7bdf
yueting <ytliaorpi@gmail.com>
2025-04-05 11:46:42 +0200

    Update process-gallery.js

--- process-gallery.js
+++ process-gallery.js
@@ -4,15 +4,16 @@ import fs from "fs";
 import path from "path";
 
 // === CONFIG ===
-const SOURCE_FOLDER_PATH = "public/images/gallery/set2";
-const inputDir = path.resolve(SOURCE_FOLDER_PATH);
+const SOURCE_FOLDER_PATH = "public/images/gallery/set1";
+
+const inputDir = path.resolve(SOURCE_FOLDER_PATH + "/source");
 const outputPreviewDir = path.resolve(SOURCE_FOLDER_PATH + "/preview");
 const outputThumbDir = path.resolve(SOURCE_FOLDER_PATH + "/thumbs");
 
 const MAX_INPUT_MB = 10;
 const QUALITY = 80;
 const PREVIEW_RESIZED_WIDTH = 1600;
-const THUMBNAIL_RESIZED_WIDTH = 600;
+const THUMBNAIL_RESIZED_HEIGHT = 500;
 
 const ensureDir = (dir) => {
   if (!fs.existsSync(dir)) fs.mkdirSync(dir, { recursive: true });
@@ -35,7 +36,7 @@ const processImage = async (file) => {
   const previewPath = path.join(outputPreviewDir, `${baseName}.webp`);
   if (!fs.existsSync(previewPath)) {
     await sharp(inputPath)
-      .resize({ width: PREVIEW_RESIZED_WIDTH })
+      //   .resize({ width: PREVIEW_RESIZED_WIDTH })
       .webp({ quality: QUALITY })
       .toFile(previewPath);
     console.log(`✅ Created preview: ${previewPath}`);
@@ -48,7 +49,9 @@ const processImage = async (file) => {
   const thumbPath = path.join(outputThumbDir, `${baseName}.webp`);
   if (!fs.existsSync(thumbPath)) {
     await sharp(inputPath)
-      .resize({ width: THUMBNAIL_RESIZED_WIDTH })
+      .resize(600, 400, {
+        fit: "cover", // crop to fill box
+      })
       .webp({ quality: QUALITY })
       .toFile(thumbPath);
     console.log(`✅ Created thumbnail: ${thumbPath}`);
