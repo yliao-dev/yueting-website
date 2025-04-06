93ebba293b03252332c03ce207962d9b59582642
yueting <ytliaorpi@gmail.com>
2025-04-07 00:42:49 +0200

    Auto scroll to top when redirect

--- src/components/layouts/MainLayout.tsx
+++ src/components/layouts/MainLayout.tsx
@@ -1,10 +1,12 @@
 import Navbar from "../shared/Navbar";
 import Footer from "../shared/Footer";
 import { Outlet } from "react-router-dom";
+import ScrollToTop from "../shared/ScrollToTop";
 
 const MainLayout = () => {
   return (
     <>
+      <ScrollToTop />
       <Navbar />
       <Outlet />
       <Footer />
