e55fc0056e620e7203181c97e0d049a171f3dd7a
yueting <ytliaorpi@gmail.com>
2025-04-06 17:03:26 +0200

    add photoviewer to the slideshwo

--- src/components/portfolio/ProjectSlideShow.tsx
+++ src/components/portfolio/ProjectSlideShow.tsx
@@ -1,20 +1,33 @@
 import { useState } from "react";
+import PhotoViewer from "../gallery/PhotoViewer";
 
 const ProjectSlideshow = ({ images }: { images: string[] }) => {
   const [current, setCurrent] = useState(0);
-
   const prev = () =>
     setCurrent((prev) => (prev === 0 ? images.length - 1 : prev - 1));
-
   const next = () =>
     setCurrent((prev) => (prev === images.length - 1 ? 0 : prev + 1));
 
+  const [selectedImage, setSelectedImage] = useState<string | null>(null);
+  const openViewer = (imageUrl: string) => {
+    setSelectedImage(imageUrl);
+  };
+  const closeViewer = () => {
+    console.log("Closing viewer");
+
+    setSelectedImage(null);
+  };
   return (
     <div className="slideshow">
+      {selectedImage && (
+        <PhotoViewer imageUrl={selectedImage} onClose={closeViewer} />
+      )}
+
       <img
         src={images[current]}
         alt={`Slide ${current + 1}`}
         className="slideshow__image"
+        onClick={() => openViewer(images[current])}
       />
 
       <div className="slideshow__controls">
