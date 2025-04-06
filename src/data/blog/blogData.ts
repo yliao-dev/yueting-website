7e60fb0d706fe8545f1e7561e5670fa6a5fdd0e0
yueting <ytliaorpi@gmail.com>
2025-04-06 21:01:21 +0200

    Test blog post

--- src/data/blog/blogData.ts
+++ src/data/blog/blogData.ts
@@ -1,76 +1,22 @@
 export const BlogData = [
-  // Kendo
   {
     id: "1",
-    title: "Kendo New York",
+    title: "title1",
     description:
       "In the heart of the forest, tall trees stand like guardians of nature.",
-    image: "images/blog/blogPost1.jpg",
     category: "kendo",
+    date: "2024-12-01",
+    coverImage: "/images/blog/post1/preview/001.webp",
+    mdPath: "src/data/blog/post1.md",
   },
   {
     id: "2",
-    title: "Kendo Florida",
-    description: "The sun rises slowly over the hills.",
-    image: "images/blog/blogPost1.jpg",
-    category: "kendo",
-  },
-  {
-    id: "3",
-    title: "Kendo California",
-    description:
-      "The forest is a peaceful place filled with tall trees and soft sounds.",
-    image: "images/blog/blogPost1.jpg",
-    category: "kendo",
-  },
-
-  // Photography
-  {
-    id: "74",
-    title: "Photography New York",
+    title: "title2",
     description:
       "In the heart of the forest, tall trees stand like guardians of nature.",
-    image: "images/blog/blogPost2.jpg",
     category: "photography",
-  },
-  {
-    id: "5",
-    title: "Photography Florida",
-    description: "The sun rises slowly over the hills.",
-    image: "images/blog/blogPost2.jpg",
-    category: "photography",
-  },
-  {
-    id: "6",
-    title: "Photography California",
-    description:
-      "The forest is a peaceful place filled with tall trees and soft sounds.",
-    image: "images/blog/blogPost2.jpg",
-    category: "photography",
-  },
-
-  // Fitness
-  {
-    id: "7",
-    title: "Fitness New York",
-    description:
-      "In the heart of the forest, tall trees stand like guardians of nature.",
-    image: "images/blog/blogPost3.jpg",
-    category: "fitness",
-  },
-  {
-    id: "8",
-    title: "Fitness Florida",
-    description: "The sun rises slowly over the hills.",
-    image: "images/blog/blogPost3.jpg",
-    category: "fitness",
-  },
-  {
-    id: "9",
-    title: "Fitness California",
-    description:
-      "The forest is a peaceful place filled with tall trees and soft sounds.",
-    image: "images/blog/blogPost3.jpg",
-    category: "fitness",
+    date: "2024-12-01",
+    coverImage: "/images/blog/post2/preview/001.webp",
+    mdPath: "src/data/blog/post2.md",
   },
 ];
