ba78624b6b3a755908d754a04a25ae1a08c628bb
yueting <ytliaorpi@gmail.com>
2025-04-07 13:02:33 +0200

    Clean post md

--- src/data/blog/blogData.ts
+++ src/data/blog/blogData.ts
@@ -1,10 +1,11 @@
 export const BlogData = [
   {
     id: "1",
-    title: "Title1",
+    title:
+      "Kendo A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with",
     author: "Yueting Liao",
     description:
-      "In the heart of the forest, tall trees stand like guardians of nature. In the heart of the forest, tall trees stand like guardians of nature In the heart of the forest, tall trees stand like guardians of nature. In the heart of the forest, tall trees stand like guardians of nature In the heart of the forest, tall trees stand like guardians of nature. In the heart of the forest, tall trees stand like guardians of nature",
+      "A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart. I am alone, and feel the charm of existence in this spot, which was created for the bliss of souls like mine. I am so happy, my dear friend, so absorbed in the exquisite sense of mere tranquil existence",
     category: "kendo",
     date: "2024-12-01",
     coverImage: "/images/blog/post1/preview/001.webp",
