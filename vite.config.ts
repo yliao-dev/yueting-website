deda0521815a5aaf058ba1ebd1bf62d43afd6f71
yueting <ytliaorpi@gmail.com>
2025-04-07 16:05:41 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,15 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   base: "/",
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ["react", "react-dom"],
+          markdown: ["react-markdown", "front-matter"],
+          mui: ["@mui/material", "@mui/icons-material"],
+        },
+      },
+    },
+  },
 });
