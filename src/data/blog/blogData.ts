c86906a23c362cbfb4588d03a36ab79559523f85
yueting <ytliaorpi@gmail.com>
2025-04-07 17:15:24 +0200

    Update blogData.ts

--- src/data/blog/blogData.ts
+++ src/data/blog/blogData.ts
@@ -8,7 +8,7 @@ export const BlogData = [
       "A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart. I am alone, and feel the charm of existence in this spot, which was created for the bliss of souls like mine. I am so happy, my dear friend, so absorbed in the exquisite sense of mere tranquil existence",
     category: "kendo",
     date: "2024-12-01",
-    coverImage: "/images/blog/post1/preview/001.webp",
+    coverImage: "/images/blog/post1/thumbs/001.webp",
     mdPath: "/src/data/blog/post1.md",
   },
   {
@@ -19,7 +19,7 @@ export const BlogData = [
       "In the heart of the forest, tall trees stand like guardians of nature.",
     category: "photography",
     date: "2024-12-01",
-    coverImage: "/images/blog/post2/preview/001.webp",
+    coverImage: "/images/blog/post2/thumbs/001.webp",
     mdPath: "/src/data/blog/post2.md",
   },
   {
@@ -31,7 +31,7 @@ export const BlogData = [
       "A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart. I am alone, and feel the charm of existence in this spot, which was created for the bliss of souls like mine. I am so happy, my dear friend, so absorbed in the exquisite sense of mere tranquil existence",
     category: "kendo",
     date: "2024-12-01",
-    coverImage: "/images/blog/post3/preview/001.webp",
+    coverImage: "/images/blog/post3/thumbs/001.webp",
     mdPath: "/src/data/blog/post3.md",
   },
 ];
