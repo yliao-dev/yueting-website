89c80414bf99d9d618168dec2002028d74f07046
yueting <ytliaorpi@gmail.com>
2025-04-08 16:24:17 +0200

    better arrow ui

--- src/pages/GalleryPage.tsx
+++ src/pages/GalleryPage.tsx
@@ -45,16 +45,20 @@ const GalleryPage = () => {
         ))}
 
       <section className="gallery__pagination">
-        {currentPage > 1 && (
-          <Link to={`/gallery/${currentPage - 1}`} className="gallery-arrow">
-            &#10094;
-          </Link>
-        )}
-        {startIndex + collectionsPerPage < GalleryData.length && (
-          <Link to={`/gallery/${currentPage + 1}`} className="gallery-arrow">
-            &#10095;
-          </Link>
-        )}
+        <div>
+          {currentPage > 1 && (
+            <Link to={`/gallery/${currentPage - 1}`} className="gallery-arrow">
+              &#10094;
+            </Link>
+          )}
+        </div>
+        <div>
+          {startIndex + collectionsPerPage < GalleryData.length && (
+            <Link to={`/gallery/${currentPage + 1}`} className="gallery-arrow">
+              &#10095;
+            </Link>
+          )}
+        </div>
       </section>
     </div>
   );
