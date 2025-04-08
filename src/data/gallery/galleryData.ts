ab0b2dcf79b09bd46fe7f1d39692685fb91e732f
yueting <ytliaorpi@gmail.com>
2025-04-08 14:55:50 +0200

    Add more images to gallery

--- src/data/gallery/galleryData.ts
+++ src/data/gallery/galleryData.ts
@@ -1,5 +1,10 @@
-import meta1 from "./galleryMeta1.json";
-import meta2 from "./galleryMeta2.json";
+import meta1 from "./collection1Meta.json";
+import meta2 from "./collection2Meta.json";
+import meta3 from "./collection3Meta.json";
+import meta4 from "./collection4Meta.json";
+import meta5 from "./collection5Meta.json";
+import meta6 from "./collection6Meta.json";
+
 import { createGalleryCollections } from "./createGalleryCollections";
 import { GalleryCollectionProps } from "../../components/gallery/galleryTypes";
 
@@ -13,5 +18,30 @@ export const GalleryData: GalleryCollectionProps[] = createGalleryCollections([
     title: "",
     slug: "collection2",
     meta: meta2,
+  },
+  {
+    title: "",
+    slug: "collection3",
+    meta: meta2,
+  },
+  {
+    title: "",
+    slug: "collection4",
+    meta: meta3,
+  },
+  {
+    title: "",
+    slug: "collection4",
+    meta: meta4,
+  },
+  {
+    title: "",
+    slug: "collection5",
+    meta: meta5,
+  },
+  {
+    title: "",
+    slug: "collection6",
+    meta: meta6,
   },
 ]);
