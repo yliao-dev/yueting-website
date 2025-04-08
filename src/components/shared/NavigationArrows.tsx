0a81519242297b86b1983f219e1b49ec3ed61da3
yueting <ytliaorpi@gmail.com>
2025-04-08 16:56:27 +0200

    Update NavigationArrows.tsx

--- src/components/shared/NavigationArrows.tsx
+++ src/components/shared/NavigationArrows.tsx
@@ -22,28 +22,29 @@ const NavigationArrows = ({
   className = "",
 }: NavigationArrowsProps) => {
   return (
-    <div className={`nav-arrows ${className}`}>
+    // NavigationArrows.tsx (better named as NavigationArrows)
+    <div className={`navigation-arrows ${className}`}>
       <div>
         {prev?.to ? (
-          <Link className="nav-arrow" to={prev.to}>
+          <Link className="navigation-arrow" to={prev.to}>
             &#10094; {prev.label}
           </Link>
         ) : prev?.onClick ? (
-          <button className="nav-arrow" onClick={prev.onClick}>
+          <button className="navigation-arrow" onClick={prev.onClick}>
             &#10094; {prev.label}
           </button>
         ) : null}
       </div>
 
-      {center && <div className="nav-center">{center}</div>}
+      {center && <div className="navigation-center">{center}</div>}
 
       <div>
         {next?.to ? (
-          <Link className="nav-arrow" to={next.to}>
+          <Link className="navigation-arrow" to={next.to}>
             {next.label} &#10095;
           </Link>
         ) : next?.onClick ? (
-          <button className="nav-arrow" onClick={next.onClick}>
+          <button className="navigation-arrow" onClick={next.onClick}>
             {next.label} &#10095;
           </button>
         ) : null}
