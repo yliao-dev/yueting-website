c4ad9e6cc8ccb38ceef2a2b31da52bdf2194a756
yueting <ytliaorpi@gmail.com>
2025-04-08 21:17:24 +0200

    use customized css

--- src/components/blog/BlogCategory.tsx
+++ src/components/blog/BlogCategory.tsx
@@ -1,9 +1,14 @@
-import { Camera, SportsMartialArts, FitnessCenter } from "@mui/icons-material";
+import { Camera, FitnessCenter } from "@mui/icons-material";
 import { BlogCategoryProps } from "./blogTypes";
+import SwordIcon from "../shared/SwordIcon";
 
 const BlogCategory = ({ onSelect }: BlogCategoryProps) => {
   const categories = [
-    { id: "kendo", icon: <SportsMartialArts />, value: "kendo" },
+    {
+      id: "kendo",
+      icon: <SwordIcon />,
+      value: "kendo",
+    },
     { id: "photography", icon: <Camera />, value: "photography" },
     { id: "fitness", icon: <FitnessCenter />, value: "fitness" },
   ];
