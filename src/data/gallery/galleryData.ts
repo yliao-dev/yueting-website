aaf32dd13373c75e5971c90c3b9ed191c50c089f
yueting <ytliaodev@gmail.com>
2025-05-03 22:22:56 +0200

    add more photos

--- src/data/gallery/galleryData.ts
+++ src/data/gallery/galleryData.ts
@@ -4,6 +4,17 @@ import meta3 from "./collection3Meta.json";
 import meta4 from "./collection4Meta.json";
 import meta5 from "./collection5Meta.json";
 import meta6 from "./collection6Meta.json";
+import meta7 from "./collection7Meta.json";
+import meta8 from "./collection8Meta.json";
+import meta9 from "./collection9Meta.json";
+import meta10 from "./collection10Meta.json";
+
+import meta11 from "./collection11Meta.json";
+import meta12 from "./collection12Meta.json";
+import meta13 from "./collection13Meta.json";
+import meta14 from "./collection14Meta.json";
+import meta15 from "./collection15Meta.json";
+import meta16 from "./collection16Meta.json";
 
 import { createGalleryCollections } from "./createGalleryCollections";
 import { GalleryCollectionProps } from "../../types/galleryTypes";
@@ -38,5 +49,55 @@ export const GalleryData: GalleryCollectionProps[] = createGalleryCollections([
     title: "",
     slug: "collection6",
     meta: meta6,
+  },
+  {
+    title: "collection7",
+    slug: "collection7",
+    meta: meta7,
+  },
+  {
+    title: "",
+    slug: "collection8",
+    meta: meta8,
+  },
+  {
+    title: "",
+    slug: "collection9",
+    meta: meta9,
+  },
+  {
+    title: "",
+    slug: "collection10",
+    meta: meta10,
+  },
+  {
+    title: "",
+    slug: "collection11",
+    meta: meta11,
+  },
+  {
+    title: "",
+    slug: "collection12",
+    meta: meta12,
+  },
+  {
+    title: "",
+    slug: "collection13",
+    meta: meta13,
+  },
+  {
+    title: "",
+    slug: "collection14",
+    meta: meta14,
+  },
+  {
+    title: "",
+    slug: "collection15",
+    meta: meta15,
+  },
+  {
+    title: "",
+    slug: "collection16",
+    meta: meta16,
   },
 ]);
