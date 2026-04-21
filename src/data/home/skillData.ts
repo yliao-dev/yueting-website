dc8fd65eba5cf7276f75da149132f513f3701cc3
yueting <ytliaodev@gmail.com>
2026-04-20 22:07:43 -0500

    🚧 Lastest

--- src/data/home/skillData.ts
+++ src/data/home/skillData.ts
@@ -10,7 +10,7 @@ export const skillsData: SkillData[] = [
   {
     label: "Coding",
     value: 35,
-    color: "#5b77a4",
+    color: "#6c6660",
     paragraphs: [
       "I like writing code to simplify life by building applications that solve real problems. This site you’re exploring is built with React and Go — keeping things fast, clean, and purposeful.",
       "Tools I like:",
@@ -20,7 +20,7 @@ export const skillsData: SkillData[] = [
   {
     label: "Kendo",
     value: 25,
-    color: "#355b8c",
+    color: "#00203f",
     paragraphs: [
       "Kendo shapes more than just physical skill—it builds presence, patience, and awareness. It’s a practice of clarity, both in movement and mindset. I started Kendo as a freshman in university, earned my shodan in early 2024, and have trained in dojos across the U.S. and around the world.",
       "My Go-to Waza:",
@@ -30,7 +30,7 @@ export const skillsData: SkillData[] = [
   {
     label: "Iaido",
     value: 10,
-    color: "#59606a",
+    color: "#8a6b53",
     paragraphs: [
       "For me, Iaido is a calm practice that sharpens precision and control. I began learning Musō Jikiden Eishin-ryū in Salt Lake City, Utah, with a brief experience in Toyama Ryū in Orem. Though still early in my journey, it complements my path in Kendo and deepens my appreciation for disciplined movement.",
     ],
@@ -38,7 +38,7 @@ export const skillsData: SkillData[] = [
   {
     label: "Fitness",
     value: 20,
-    color: "#6f8754",
+    color: "#5e7154",
     paragraphs: [
       "My training centers on strength, explosiveness, and stability. Injury prevention comes before aesthetics, and athletic performance over appearance. Most of it happens with just a barbell.",
       "My Go-to lifts:",
@@ -54,7 +54,7 @@ export const skillsData: SkillData[] = [
   {
     label: "Photography",
     value: 15,
-    color: "#8b6748",
+    color: "#6e7f8e",
     paragraphs: [
       "Photography lets me chase light, frame feeling, and preserve moments that might quietly disappear.",
       "My usual captures:",
